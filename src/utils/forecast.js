6260c8d1684d88df01b46eb9c700723a1a117873
Harisanker A <rahul.raj@thinkcoretech.com>
2019-08-18 10:15:53 +0530

    Added high and low temprature in the forecast result

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,9 @@ const forecast = (longitude, lattitude, callback) => {
             callback('Unable to find the location', undefined)
         }
         else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.');
+            console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.'
+            + ' The high temprature today is ' + body.daily.data[0].temperatureHigh  + ' with low ' + body.daily.data[0].temperatureLow);
         }
     })
 }
